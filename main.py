3c138ae3909dfd1f7f251ed7b21f6d703af4c1b4
Jay <lelerukjaymoh@gmail.com>
2023-03-31 16:51:29 +0300

    Extract data from the contracts parquet file

---
+++ main.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import polars as pl
+import json
+
+data_path = "/home/jay/projects/personal/paradigm/data/ethereum_contracts__v1_0_0__00000000_to_00999999.parquet"
+
+
+def save_to_json(contracts):
+    with open('contracts.json', 'w') as f:
+        json.dump(contracts, f)
+
+
+def getContracts():
+    column = 'contract_address'
+    col = '0x' + (pl.col(column).struct.field(column).bin.encode('hex'))
+
+    contracts = []
+
+    data = (
+        pl.scan_parquet(data_path)
+        .select(pl.col(column).value_counts())
+        .select([col.alias(column)])
+        .collect(streaming=True)
+    )['contract_address']
+
+    for binary_address in data:
+        address = ctc.binary_convert(binary_address, 'prefix_hex')
+        contracts.append(address)
+
+    save_to_json(contracts)
+
+
+getContracts()
